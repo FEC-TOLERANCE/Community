983023255b3dc6aee15188e0c02962cd60a4bd76
Teddy Kim <teddykim09@gmail.com>
2020-09-25 16:02:19 -0700

    Set up all dependencies and basic react page

---
+++ client/src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>Start of Community</div>
+    )
+  }
+}
+
+export default App;
