e6286a8261eacf52943a01aa630fae3db772d586
Teddy Kim <teddykim09@gmail.com>
2020-10-07 17:25:24 -0700

    Fixed webpack entry point and refactored App to Community to use a function component

---
+++ client/src/Community.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+function Community() {
+  return (
+    <div>Start of Community</div>
+  )
+}
+
+ReactDOM.render(<Community />, document.getElementById('community'));
+
+export default Community;
