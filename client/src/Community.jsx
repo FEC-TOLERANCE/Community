60e6be44d68b902c8d4e7dd52df32d021a8d9cf4
Teddy Kim <teddykim09@gmail.com>
2020-10-12 18:49:45 -0700

    Added new component for Community to dynamically render country data based on project id

--- client/src/Community.jsx
+++ client/src/Community.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Origin from './Components/Origin.jsx';
 
 function Community() {
+  const [backers, setBackers] = useState(0);
+  const [locations, setLocation] = useState([]);
+
+  useEffect(() => {
+    let currentUrl = window.location.href.split('/');
+    let url = currentUrl[0] + '//' + currentUrl[2], projectId = currentUrl[3];
+
+    axios.get(`${url}/community/${projectId}`)
+      .then(locations => {
+        setLocation(locations.data);
+      })
+      .catch(err => {
+        throw new Error(err);
+      })
+  }, [])
+
   return (
-    <div>Start of Community</div>
+    <div>
+      <Origin locations = {locations}/>
+    </div>
   )
 }
 
 ReactDOM.render(<Community />, document.getElementById('community'));
-
-export default Community;
